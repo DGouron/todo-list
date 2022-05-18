61e6a75f97bf23467f0f2acf7b307a4502402ce2
DGouron <damien.gouron@gmail.com>
2022-05-18 16:05:22 +0200

    Add date component

---
+++ src/components/Header/CurrentDate.jsx
@@ -0,0 +1,44 @@
+import {React} from 'react';
+
+function CurrentDate(){
+    const currentDate = new Date();
+    console.log(currentDate.getMonth())
+    const displayDate = `${translateDayName(currentDate.getDay())} ${currentDate.getDate()} ${translateMonthName(currentDate.getMonth())}`
+    
+    translateDayName('');
+    return <small>{displayDate}</small>
+}
+
+function translateDayName(dayName){
+    switch(dayName){
+        case 1 : return "Lun";
+        case 2 : return "Mar";
+        case 3 : return "Mer";
+        case 4 : return "Jeu";
+        case 5 : return "Ven";
+        case 6 : return "Sam";
+        case 7 : return "Dim";
+        default : return "";
+    }
+}
+
+function translateMonthName(monthName){
+    switch(monthName + 1){
+        case 1 : return "janvier";
+        case 2 : return "février";
+        case 3 : return "mars";
+        case 4 : return "avril";
+        case 5 : return "mai";
+        case 6 : return "juin";
+        case 7 : return "juillet";
+        case 8 : return "août";
+        case 9 : return "septembre";
+        case 10 : return "octobre";
+        case 11 : return "novembre";
+        case 12 : return "décembre";
+        default : return "";
+    }
+}
+
+
+export default CurrentDate;
--- src/components/Header/Title.jsx
+++ src/components/Header/Title.jsx
@@ -1,10 +1,11 @@
 import {React} from 'react';
+import CurrentDate from './CurrentDate';
 
 function Title(){
     const title = "Aujourd'hui ";
     return ( 
     <div className='view_header'>
-        <h2>{title}<small>Sam 14 mai</small></h2>
+        <h2>{title}<CurrentDate /></h2>
     </div>
     );
 }

e4a2b92708da3c007a1b41895497cdb3fe13bb15
DGouron <damien.gouron@gmail.com>
2022-05-20 17:37:25 +0200

    ToDoList animation/design

--- src/components/AddTaskPannelView/AddTaskButton.jsx
+++ src/components/AddTaskPannelView/AddTaskButton.jsx
@@ -1,31 +1,35 @@
 import {React} from 'react';
 import PropTypes from 'prop-types';
 
-function AddTaskButton({taskName, taskDescription, setNewTask, deactiveThisButton}){
+function AddTaskButton({taskName, taskDescription, setNewTask, deactiveThisButton, setCallClearForm}){
     return ( 
     <div className=''>
         {deactiveThisButton
             ?
-            <input type={"button"} className={"empty-state-button"} defaultValue={"Ajouter une tâche"} onClick={() => handlerClick(taskName, taskDescription, setNewTask)} disabled={true}/>
+            <input type={"button"} className={"empty-state-button"} defaultValue={"Ajouter une tâche"} disabled={deactiveThisButton} onClick={() => handlerClick(taskName, taskDescription, setNewTask)}/>
  
             :
-            <input type={"button"} className={"empty-state-button"} defaultValue={"Ajouter une tâche"} onClick={() => handlerClick(taskName, taskDescription, setNewTask)}/>
+            <input type={"button"} className={"empty-state-button"} defaultValue={"Ajouter une tâche"} onClick={() =>{
+                 handlerClick(taskName, taskDescription, setNewTask, setCallClearForm)}}/>
  
         }
     </div>
     );
 }
 
-function handlerClick(taskName, taskDescription, setNewTask){
+function handlerClick(taskName, taskDescription, setNewTask, setCallClearForm){
+    setCallClearForm(true);
     let newTask = {
         taskId: taskName + Math.random(),
         taskName: taskName,
         taskDescription: taskDescription
     };
     setNewTask(newTask);
+    console.log('CALL CLEAR')
 }
 
 AddTaskButton.propTypes = {
+    setCallClearForm: PropTypes.func,
     deactiveThisButton: PropTypes.bool,
     taskName: PropTypes.string,
     taskDescription: PropTypes.string,
--- src/components/AddTaskPannelView/AddTaskPannelView.jsx
+++ src/components/AddTaskPannelView/AddTaskPannelView.jsx
@@ -1,11 +1,24 @@
-import {React, useState} from 'react';
+import {React, useEffect, useState} from 'react';
 import AddTaskButton from './AddTaskButton';
 import PropTypes from 'prop-types';
 
 function AddTaskPannelView( {toDoListItems, setNewTask, isEditorOpen, setIsEditorOpen}){
-   const minLength = 4;
+
+    const minLength = 4;
     const [taskNameValue, setTaskNameValue] = useState('');
     const [taskDescription, setTaskDescriptionValue] = useState('');
+    const [formMustBeClear, setCallClearForm] = useState(true);
+
+    useEffect(() => {
+        console.log(formMustBeClear)
+        if(formMustBeClear){ //Autoclose action
+                setTaskNameValue('');
+                setTaskDescriptionValue('');
+
+                setCallClearForm(false);
+        }
+    }, [formMustBeClear])
+
 
     return (
         <div>
@@ -15,18 +28,26 @@ function AddTaskPannelView( {toDoListItems, setNewTask, isEditorOpen, setIsEdito
                 <form className='focus-marker-enabled-within'>
                     <div className='task_editor__editing_area'>
                         <div className='task_editor__input_fields'>   
-                            <input name = {"taskNameField"} onChange={(e) => setTaskNameValue(e.target.value)} type={"text"} placeholder={"ex. : Déjeuner en famille dimanche à 12h #Personnel"} className='task_editor__content_field' />
-                            <textarea placeholder='Description' onChange={(e) => setTaskDescriptionValue(e.target.value)}></textarea>
+                            <input name = {"taskNameField"} value={taskNameValue} onChange={(e) => {
+                                setTaskNameValue(e.target.value)}} type={"text"} placeholder={"ex. : Déjeuner en famille dimanche à 12h #Personnel"} className='task_editor__content_field' />
+                            <textarea placeholder='Description' value={taskDescription} onChange={(e) => { 
+                                setTaskDescriptionValue(e.target.value)}}></textarea>
                         </div> 
                     </div>   
                 </form>
                 <div className='addTaskButtonsRow'>
-                <AddTaskButton taskName={taskNameValue} taskDescription={taskDescription} toDoListItems = {toDoListItems} setNewTask = {setNewTask} deactiveThisButton={taskNameValue.length < minLength || taskDescription.length < minLength}/>
+                <AddTaskButton 
+                    taskName={taskNameValue} 
+                    taskDescription={taskDescription} 
+                    toDoListItems = {toDoListItems} 
+                    setCallClearForm={setCallClearForm} 
+                    setNewTask = {setNewTask} 
+                    deactiveThisButton={taskNameValue.length < minLength || taskDescription.length < minLength}/>
                 <button className={"_7a2031d6 _81c213d2 _949f7858 f9408a0e _56a651f6"}  onClick = {() => handleOpenEditorButtonClick(setIsEditorOpen, false)}> <span className={"_09c23660"}>Annuler</span></button>
             </div>
         </div>
     : 
-    <input type={"button"} defaultValue={"Ajouter une tâche"} name = {"OpenEditor"} className={"empty-state-button"}onClick = {() => handleOpenEditorButtonClick(setIsEditorOpen, true)}/>
+    <input type={"button"} value={"Ajouter une tâche"} name = {"OpenEditor"} className={"empty-state-button"}onClick = {() => handleOpenEditorButtonClick(setIsEditorOpen, true)}/>
     }   
         </div>)
 }
